1a4248404314405ba97d12e59427b851271be5f2
sway2020 <65274619+sway2020@users.noreply.github.com>
2020-10-27 15:57:33 -0700

    Update translations

--- FindIt/ModInfo.cs
+++ FindIt/ModInfo.cs
@@ -13,7 +13,7 @@ namespace FindIt
 {
     public class ModInfo : IUserMod
     {
-        public const string version = "2.0.7-beta3";
+        public const string version = "2.0.7-beta4";
         public const bool isBeta = true;
         public const bool showExtraDebuggingMessage = false;
 
