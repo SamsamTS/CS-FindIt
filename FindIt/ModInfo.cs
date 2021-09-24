29544fbd6bb2e1d2fd09bc39c8d5e38ce07f09dc
sway2020 <65274619+sway2020@users.noreply.github.com>
2021-09-23 21:43:45 -0700

    v2.7.1
    - Add new search prefix $ to exclude assets with a specific custom tag
    
    Example: $test_tag
    
    Assets tagged with the test_tag custom tag will be excluded
    You can use it to search for new assets that are not tagged
    
    - Fix thumbnail errors caused by some very old workshop building assets
    
    - Update translations

--- FindIt/ModInfo.cs
+++ FindIt/ModInfo.cs
@@ -13,9 +13,9 @@ namespace FindIt
     public class ModInfo : IUserMod
     {
         public const string version = "2.7.1";
-        public const bool isBeta = true;
+        public const bool isBeta = false;
         public const bool debug = false;
-        public const double updateNoticeDate = 20210918;
+        public const double updateNoticeDate = 20210923;
         public const string updateNotice =
 
             "- Add new search prefix $ to exclude assets with a specific custom tag\n\n" +
@@ -24,7 +24,7 @@ namespace FindIt
             "  Assets tagged with the test_tag custom tag will be excluded\n\n" +
             "  You can use it to find new assets that are not tagged\n\n" + 
 
-            "- Fix thumbnail errors caused by ancient workshop building assets\n\n" +
+            "- Fix thumbnail errors caused by some very old workshop building assets\n\n" +
 
             "- Update translations\n\n";
 
